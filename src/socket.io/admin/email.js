e0aa29fa049252e3a6ee642dc5a5f11f85fd63c0
Ellen Fang <ellenfang77@Ellens-MacBook-Pro-6.local>
2023-09-04 18:08:11 -0400

    add test property and as notif type

--- src/socket.io/admin/email.js
+++ src/socket.io/admin/email.js
@@ -11,133 +11,72 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 const meta_1 = __importDefault(require("../../meta"));
 const digest_1 = __importDefault(require("../../user/digest"));
 const email_1 = __importDefault(require("../../user/email"));
 const notifications_1 = __importDefault(require("../../notifications"));
 const emailer_1 = __importDefault(require("../../emailer"));
 const utils_1 = __importDefault(require("../../utils"));
-// const Email: EmailTemplate = {
-//     type: '',
-//     bodyShort: '',
-//     bodyLong: '',
-//     nid: '',
-//     path: '',
-//     from: 0,
-//     test: function(){},
-// };
-// Email.test = async function (socket: Socket, data: { payload?: Payload; template: string }): Promise<void> {
-//     const payload: Payload = {
-//         ...(data.payload || {}),
-//         subject: '[[email:test-email.subject]]',
-//     };
-//     switch (data.template) {
-//     case 'digest':
-//         await userDigest.execute({
-//             interval: 'month',
-//             subscribers: [socket.uid],
-//         });
-//         break;
-//     case 'banned':
-//         Object.assign(payload, {
-//             username: 'test-user',
-//             until: utils.toISOString(Date.now()) as string,
-//             reason: 'Test Reason',
-//         });
-//         await emailer.send(data.template, socket.uid, payload);
-//         break;
-//     case 'verify-email':
-//     case 'welcome':
-//         await userEmail.sendValidationEmail(socket.uid, {
-//             force: 1,
-//             template: data.template,
-//             subject:
-//                 data.template === 'welcome' ?
-//                     `[[email:welcome-to, ${meta.config.title as string
-// || meta.config.browserTitle as string || 'NodeBB'}]]` :
-//                     undefined,
-//         });
-//         break;
-//     case 'notification': {
-//         const notification: notif = await notifications.create({
-//             type: 'test',
-//             bodyShort: '[[email:notif.test.short]]',
-//             bodyLong: '[[email:notif.test.long]]',
-//             nid: `uid:${socket.uid}:test`,
-//             path: '/',
-//             from: socket.uid,
-//         });
-//         await emailer.send('notification', socket.uid, {
-//             path: notification.path,
-//             subject: utils.stripHTMLTags(notification.subject || '[[notifications:new_notification]]'),
-//             intro: utils.stripHTMLTags(notification.bodyShort),
-//             body: notification.bodyLong || '',
-//             notification,
-//             showUnsubscribe: true,
-//         });
-//         break;
-//     }
-//     default:
-//         await emailer.send(data.template, socket.uid, payload);
-//         break;
-//     }
-// };
-class Email {
-    static test(socket, data) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const payload = Object.assign(Object.assign({}, (data.payload || {})), { subject: '[[email:test-email.subject]]' });
-            switch (data.template) {
-                case 'digest':
-                    yield digest_1.default.execute({
-                        interval: 'month',
-                        subscribers: [socket.uid],
-                    });
-                    break;
-                case 'banned':
-                    Object.assign(payload, {
-                        username: 'test-user',
-                        until: utils_1.default.toISOString(Date.now()),
-                        reason: 'Test Reason',
-                    });
-                    yield emailer_1.default.send(data.template, socket.uid, payload);
-                    break;
-                case 'verify-email':
-                case 'welcome':
-                    yield email_1.default.sendValidationEmail(socket.uid, {
-                        force: 1,
-                        template: data.template,
-                        subject: data.template === 'welcome' ?
-                            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-                            `[[email:welcome-to, ${meta_1.default.config.title || meta_1.default.config.browserTitle || 'NodeBB'}]]` :
-                            undefined,
-                    });
-                    break;
-                case 'notification': {
-                    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-                    const notification = yield notifications_1.default.create({
-                        type: 'test',
-                        bodyShort: '[[email:notif.test.short]]',
-                        bodyLong: '[[email:notif.test.long]]',
-                        nid: `uid:${socket.uid}:test`,
-                        path: '/',
-                        from: socket.uid,
-                    });
-                    yield emailer_1.default.send('notification', socket.uid, {
-                        path: notification.path,
-                        subject: utils_1.default.stripHTMLTags(notification.subject || '[[notifications:new_notification]]'),
-                        intro: utils_1.default.stripHTMLTags(notification.bodyShort),
-                        body: notification.bodyLong || '',
-                        notification,
-                        showUnsubscribe: true,
-                    });
-                    break;
-                }
-                default:
-                    yield emailer_1.default.send(data.template, socket.uid, payload);
-                    break;
+const Email = {
+    type: '',
+    bodyShort: '',
+    bodyLong: '',
+    nid: '',
+    path: '',
+    from: 0,
+};
+Email.test = function (socket, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const payload = Object.assign(Object.assign({}, (data.payload || {})), { subject: '[[email:test-email.subject]]' });
+        switch (data.template) {
+            case 'digest':
+                yield digest_1.default.execute({
+                    interval: 'month',
+                    subscribers: [socket.uid],
+                });
+                break;
+            case 'banned':
+                Object.assign(payload, {
+                    username: 'test-user',
+                    until: utils_1.default.toISOString(Date.now()),
+                    reason: 'Test Reason',
+                });
+                yield emailer_1.default.send(data.template, socket.uid, payload);
+                break;
+            case 'verify-email':
+            case 'welcome':
+                yield email_1.default.sendValidationEmail(socket.uid, {
+                    force: 1,
+                    template: data.template,
+                    subject: data.template === 'welcome' ?
+                        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                        `[[email:welcome-to, ${meta_1.default.config.title || meta_1.default.config.browserTitle || 'NodeBB'}]]` :
+                        undefined,
+                });
+                break;
+            case 'notification': {
+                const notification = yield notifications_1.default.create({
+                    type: 'test',
+                    bodyShort: '[[email:notif.test.short]]',
+                    bodyLong: '[[email:notif.test.long]]',
+                    nid: `uid:${socket.uid}:test`,
+                    path: '/',
+                    from: socket.uid,
+                });
+                yield emailer_1.default.send('notification', socket.uid, {
+                    path: notification.path,
+                    subject: utils_1.default.stripHTMLTags(notification.subject || '[[notifications:new_notification]]'),
+                    intro: utils_1.default.stripHTMLTags(notification.bodyShort),
+                    body: notification.bodyLong || '',
+                    notification,
+                    showUnsubscribe: true,
+                });
+                break;
             }
-        });
-    }
-}
-exports.default = Email;
+            default:
+                yield emailer_1.default.send(data.template, socket.uid, payload);
+                break;
+        }
+    });
+};
+module.exports = Email;
--- src/socket.io/admin/email.ts
+++ src/socket.io/admin/email.ts
@@ -5,14 +5,6 @@ import notifications from '../../notifications';
 import emailer from '../../emailer';
 import utils from '../../utils';
 
-// interface Config {
-//     title: string;
-//     browserTitle: string;
-// }
-// interface Meta {
-//     config: Config
-// }
-
 interface Socket {
     uid: number;
 }
@@ -34,141 +26,85 @@ interface Payload {
     reason?: string;
 }
 
-// const Email: EmailTemplate = {
-//     type: '',
-//     bodyShort: '',
-//     bodyLong: '',
-//     nid: '',
-//     path: '',
-//     from: 0,
-//     test: function(){},
-// };
-
-// Email.test = async function (socket: Socket, data: { payload?: Payload; template: string }): Promise<void> {
-//     const payload: Payload = {
-//         ...(data.payload || {}),
-//         subject: '[[email:test-email.subject]]',
-//     };
-
-//     switch (data.template) {
-//     case 'digest':
-//         await userDigest.execute({
-//             interval: 'month',
-//             subscribers: [socket.uid],
-//         });
-//         break;
-
-//     case 'banned':
-//         Object.assign(payload, {
-//             username: 'test-user',
-//             until: utils.toISOString(Date.now()) as string,
-//             reason: 'Test Reason',
-//         });
-//         await emailer.send(data.template, socket.uid, payload);
-//         break;
-
-//     case 'verify-email':
-//     case 'welcome':
-//         await userEmail.sendValidationEmail(socket.uid, {
-//             force: 1,
-//             template: data.template,
-//             subject:
-//                 data.template === 'welcome' ?
-//                     `[[email:welcome-to, ${meta.config.title as string
-// || meta.config.browserTitle as string || 'NodeBB'}]]` :
-//                     undefined,
-//         });
-//         break;
-
-//     case 'notification': {
-//         const notification: notif = await notifications.create({
-//             type: 'test',
-//             bodyShort: '[[email:notif.test.short]]',
-//             bodyLong: '[[email:notif.test.long]]',
-//             nid: `uid:${socket.uid}:test`,
-//             path: '/',
-//             from: socket.uid,
-//         });
-//         await emailer.send('notification', socket.uid, {
-//             path: notification.path,
-//             subject: utils.stripHTMLTags(notification.subject || '[[notifications:new_notification]]'),
-//             intro: utils.stripHTMLTags(notification.bodyShort),
-//             body: notification.bodyLong || '',
-//             notification,
-//             showUnsubscribe: true,
-//         });
-//         break;
-//     }
-
-//     default:
-//         await emailer.send(data.template, socket.uid, payload);
-//         break;
-//     }
-// };
-
-class Email {
-    static async test(socket: Socket, data: { payload?: Payload; template: string }): Promise<void> {
-        const payload: Payload = {
-            ...(data.payload || {}),
-            subject: '[[email:test-email.subject]]',
-        };
-
-        switch (data.template) {
-        case 'digest':
-            await userDigest.execute({
-                interval: 'month',
-                subscribers: [socket.uid],
-            });
-            break;
-
-        case 'banned':
-            Object.assign(payload, {
-                username: 'test-user',
-                until: utils.toISOString(Date.now()) as string,
-                reason: 'Test Reason',
-            });
-            await emailer.send(data.template, socket.uid, payload);
-            break;
-
-        case 'verify-email':
-        case 'welcome':
-            await userEmail.sendValidationEmail(socket.uid, {
-                force: 1,
-                template: data.template,
-                subject:
-                    data.template === 'welcome' ?
-                    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-                        `[[email:welcome-to, ${meta.config.title as string || meta.config.browserTitle as string || 'NodeBB'}]]` :
-                        undefined,
-            });
-            break;
-
-        case 'notification': {
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-            const notification: notif = await notifications.create({
-                type: 'test',
-                bodyShort: '[[email:notif.test.short]]',
-                bodyLong: '[[email:notif.test.long]]',
-                nid: `uid:${socket.uid}:test`,
-                path: '/',
-                from: socket.uid,
-            });
-            await emailer.send('notification', socket.uid, {
-                path: notification.path,
-                subject: utils.stripHTMLTags(notification.subject || '[[notifications:new_notification]]'),
-                intro: utils.stripHTMLTags(notification.bodyShort),
-                body: notification.bodyLong || '',
-                notification,
-                showUnsubscribe: true,
-            });
-            break;
-        }
-
-        default:
-            await emailer.send(data.template, socket.uid, payload);
-            break;
-        }
-    }
+interface EmailTemplate {
+    type: string;
+    bodyShort: string;
+    bodyLong: string;
+    nid: string;
+    path: string;
+    from: number;
+    test?: (socket: Socket, data: { payload?: Payload; template: string }) => Promise<void>;
 }
 
-export default Email;
+const Email: EmailTemplate = {
+    type: '',
+    bodyShort: '',
+    bodyLong: '',
+    nid: '',
+    path: '',
+    from: 0,
+};
+
+Email.test = async function (socket: Socket, data: { payload?: Payload; template: string }): Promise<void> {
+    const payload: Payload = {
+        ...(data.payload || {}),
+        subject: '[[email:test-email.subject]]',
+    };
+
+    switch (data.template) {
+    case 'digest':
+        await userDigest.execute({
+            interval: 'month',
+            subscribers: [socket.uid],
+        });
+        break;
+
+    case 'banned':
+        Object.assign(payload, {
+            username: 'test-user',
+            until: utils.toISOString(Date.now()) as string,
+            reason: 'Test Reason',
+        });
+        await emailer.send(data.template, socket.uid, payload);
+        break;
+
+    case 'verify-email':
+    case 'welcome':
+        await userEmail.sendValidationEmail(socket.uid, {
+            force: 1,
+            template: data.template,
+            subject:
+                data.template === 'welcome' ?
+                    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+                    `[[email:welcome-to, ${meta.config.title as string || meta.config.browserTitle as string || 'NodeBB'}]]` :
+                    undefined,
+        });
+        break;
+
+    case 'notification': {
+        const notification: notif = await notifications.create({
+            type: 'test',
+            bodyShort: '[[email:notif.test.short]]',
+            bodyLong: '[[email:notif.test.long]]',
+            nid: `uid:${socket.uid}:test`,
+            path: '/',
+            from: socket.uid,
+        }) as notif;
+        await emailer.send('notification', socket.uid, {
+            path: notification.path,
+            subject: utils.stripHTMLTags(notification.subject || '[[notifications:new_notification]]'),
+            intro: utils.stripHTMLTags(notification.bodyShort),
+            body: notification.bodyLong || '',
+            notification,
+            showUnsubscribe: true,
+        });
+        break;
+    }
+
+    default:
+        await emailer.send(data.template, socket.uid, payload);
+        break;
+    }
+};
+
+export = Email;
